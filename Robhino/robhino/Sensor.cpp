a49f1cbd7378d60653a21cac6c884207109930c9
JosiasCaro <88778249+JosiasCaro@users.noreply.github.com>
2024-10-25 15:41:04 -0300

    Add files via upload

---
+++ Robhino/robhino/Sensor.cpp
@@ -0,0 +1,35 @@
+#include "Sensor.h"
+
+Sensor::Sensor(int pingPin) {
+    _pingPin = pingPin;
+};
+
+
+void Sensor::medirDistancia(int pin) {
+    long duration, cm;
+
+    pinMode(_pingPin, OUTPUT);
+    digitalWrite(_pingPin, LOW);
+    delayMicroseconds(2);
+    digitalWrite(_pingPin, HIGH);
+    delayMicroseconds(5);
+    digitalWrite(_pingPin, LOW);
+
+    pinMode(_pingPin, INPUT);
+    duration = pulseIn(_pingPin, HIGH);
+
+    cm = microsegundosACentimetros(duration);
+    if(cm < 10) {
+        digitalWrite(pin, HIGH);
+    } else {
+        digitalWrite(pin, LOW);
+    }
+
+    Serial.print(cm);
+    Serial.print("cm");
+    Serial.println();
+};
+
+long Sensor::microsegundosACentimetros(long microseconds) {
+    return microseconds / 29 / 2;
+}
---
+++ Robhino/robhino/Sensor.h
@@ -0,0 +1,18 @@
+#ifndef Sensor_h
+#define Sensor_h
+
+#include <Arduino.h>
+#include "Sensor.h"
+
+class Sensor {
+    
+    public: 
+        Sensor(int pingPin);
+        void medirDistancia(int pin);
+
+    private:
+        int _pingPin;
+        long microsegundosACentimetros(long microseconds);
+};
+
+#endif
