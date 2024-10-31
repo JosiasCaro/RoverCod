3be63d7ade99c50cb0283db15bf0b18a0da859af
Nacho <Ignacio.rizu@gmail.com>
2024-10-31 10:40:38 -0300

    Se cambia la funcionalidad del joystick para que controle el rover, en vez de que el rover controle el joystick. Se usa el & en la inicializacion del joystick en el rover para usar la instancia que se pasa como parametro en el main.

--- Robhino/robhino/robhino.ino
+++ Robhino/robhino/robhino.ino
@@ -58,6 +58,6 @@ void loop() {
   // https://stackoverflow.com/questions/66278271/task-watchdog-got-triggered-the-tasks-did-not-reset-the-watchdog-in-time
   
   rover.actualizar(choque);
-  vTaskDelay(1);
+  // vTaskDelay(1);
   delay(100);
 }
